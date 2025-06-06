4bf753af4f22d8175ef78785d331cad38d08fb58
Le Tien Tai <22120314@student.hcmus.edu.vn>
2025-06-06 15:01:23 +0700

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http")
-const app = require('./app');
-const PORT = 3000;
-
-const server = http.createServer(app);
-
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-});
