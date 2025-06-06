954ab4e801cbe9744436b28c28a2618a701bc9c8
Le Tien Tai <22120314@student.hcmus.edu.vn>
2025-06-06 15:01:11 +0700

    Delete src directory

--- src/features/documents/payment-receipt/receipt.routes.js
+++
@@ -1,9 +0,0 @@
-const { createReceipt, getReceipt } = require('./receipt.controller');
-const { checkRole } = require('../../auth/middleware');
-const { verifyToken } = require('../../auth/middleware');
-const express = require('express');
-const router = express.Router();
-
-router.post('/create', verifyToken, checkRole('staff'), createReceipt);
-router.get('/get', verifyToken, checkRole('staff'), getReceipt);
-module.exports = router;
